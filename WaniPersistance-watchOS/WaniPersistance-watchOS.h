60337338edefe3fd37ce76ade83803ce6b1e16ff
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-06-06 21:44:45 +0300

    watchOS build

---
+++ WaniPersistance-watchOS/WaniPersistance-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  WaniPersistance-watchOS.h
+//  WaniPersistance-watchOS
+//
+//  Created by Andriy K. on 6/6/17.
+//  Copyright © 2017 @haawa799. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for WaniPersistance-watchOS.
+FOUNDATION_EXPORT double WaniPersistance_watchOSVersionNumber;
+
+//! Project version string for WaniPersistance-watchOS.
+FOUNDATION_EXPORT const unsigned char WaniPersistance_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaniPersistance_watchOS/PublicHeader.h>
+
+
